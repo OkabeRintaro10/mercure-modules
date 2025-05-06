2ea6e6946b4c83aee06b34f28b348d81c8aa1061
Ryefoxlime <kausikremella@gmail.com>
2025-05-06 11:33:35 +0000

    fixing error 422

--- ApiCaller/api_call.py
+++ ApiCaller/api_call.py
@@ -38,7 +38,7 @@ def main(args=sys.argv[1:]):
         # response = requests.get("http://10.0.2.2:8001/")
         # print(response.json())
         for image_filename in series[item]:
-            files = {"image": open(os.path.join(in_folder, image_filename), "rb")}
+            files = {"files": open(os.path.join(in_folder, image_filename), "rb")}
             try:
                 response = requests.post("http://10.0.2.2:8001/upload", files=files)
                 response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
